af1e0e446386d7040d8ca0a11db11916058050b2
mete5050 <33295066+mete5050@users.noreply.github.com>
2019-03-08 21:40:13 +0300

    Add files via upload

---
+++ linux_uygulamalari.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo add-apt-repository ppa:mikhailnov/pulseeffects -y
+sudo apt-get -y install pulseeffects
+sudo apt-get -y install flashplugin-installer
+sudo apt-get -y install kdenlive
+sudo apt-get -y install gparted
+sudo apt-get -y  install hydrogen
+sudo apt-get -y  install lmms
+sudo apt-get -y  install blender
+sudo apt-get -y install arduino 
+sudo apt-get -y install eclipse
+sudo apt-get -y  install mixxx
+sudo apt-get -y  install pulseeffects
+sudo apt-get -y install vokoscreen
+sudo apt-get -y install wine
+sudo apt-get -y install chromium
+sudo apt-get -y install opera
+sudo apt-get -y install discord
+sudo apt-get -y install steam
+sudo add-apt-repository multiverse
+sudo apt update
+sudo apt-get -y install steam
+sudo pip install opencv-python
+sudo pip install autogui
+
